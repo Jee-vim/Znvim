4f92216bb512b0c508ecd2e667626526c86a0279
Jee <zeddnyx08@gmail.com>
2025-05-11 12:27:33 +0700

    refactor: update snippet html, tailwind

--- lua/zedd/plugins/cmp/snippet.lua
+++ lua/zedd/plugins/cmp/snippet.lua
@@ -1,21 +1,22 @@
 vim.g.vsnip_snippet_dir = vim.fn.stdpath("config") .. "/snippets"
 vim.g.vsnip_filetypes = {
-	javascriptreact = {
-		"javascript/react",
-		"javascript/javascript",
-		"html",
-	},
-	typescriptreact = {
-		"javascript/react",
-		"javascript/typescript",
-		"javascript/javascript",
+  javascriptreact = {
+    "javascript/react",
+    "javascript/javascript",
     "tailwindcss",
-		"html",
-	},
-	javascript = { "javascript/javascript" },
-	typescript = { "javascript/typescript", "javascript/javascript" },
-	css = { "tailwindcss" },
-	html = { "angular/html" },
-	http = { "http" },
+    "html",
+  },
+  typescriptreact = {
+    "javascript/react",
+    "javascript/javascript",
+    "javascript/typescript",
+    "tailwindcss",
+    "html",
+  },
+  javascript = { "javascript/javascript" },
+  typescript = { "javascript/typescript" },
+  css = { "tailwindcss" },
+  html = { "angular/html" },
+  http = { "http" },
   c = { "c" },
 }
