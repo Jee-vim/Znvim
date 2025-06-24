2166db37f0e8d760d76bc38c7430c4f222c088d8
Jee <zeddnyx08@gmail.com>
2025-06-25 06:45:41 +0700

    chore: show laststatus, lower cmdheight and update snippet ts. refactor: add maxServeMem on ts_ls

--- lsp/ts_ls.lua
+++ lsp/ts_ls.lua
@@ -1,6 +1,6 @@
 return {
   cmd = { "typescript-language-server", "--stdio" },
-  filetypes = { "typescript", "javascript", "typescriptreact" },
+  filetypes = { "typescript", "javascript", "typescriptreact", "--tsserver-path", "tsserver", "--maxTsServerMemory=4096" },
   handlers = {
     ["workspace/applyEdit"] = function()
       return { applied = false }
