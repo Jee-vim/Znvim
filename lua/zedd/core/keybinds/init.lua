223d5eaf5a991c277bde0ff92707932f0aae6c1d
Jee <zeddnyx08@gmail.com>
2025-08-17 17:49:33 +0700

    chore: reorganize keybind position and added new keybind for quit

--- lua/zedd/core/keybinds/init.lua
+++ lua/zedd/core/keybinds/init.lua
@@ -17,11 +17,17 @@ nmap("<A-d>", "<CMD>lua vim.diagnostic.setloclist()<CR>")
 nmap("<A-j>", "<CMD>lua vim.diagnostic.goto_next()<CR>")
 nmap("<A-k>", "<CMD>lua vim.diagnostic.goto_prev()<CR>")
 
--- Center Screen on Jumping
-nmap("n", "nzzzv")
-nmap("N", "Nzzzv")
-nmap("<C-d>", "<C-d>zz")
-nmap("<C-u>", "<C-u>zz")
+-- Git
+nmap("<leader>gd", "<CMD>Gitsign toggle_deleted<CR>")
+nmap("<leader>gcl", "<CMD>Gitsign toggle_current_line_blame<CR>")
+nmap("<leader>gbl", "<CMD>Gitsign blame_line<CR>")
+nmap("<leader>c0", "<CMD>GitConflictChooseNone<CR>")
+nmap("<leader>cb", "<CMD>GitConflictChooseBoth<CR>")
+nmap("<leader>co", "<CMD>GitConflictChooseOurs<CR>")
+nmap("<leader>ct", "<CMD>GitConflictChooseTheirs<CR>")
+nmap("<leader>cn", "<CMD>GitConflictNextConflict<CR>")
+nmap("<leader>cp", "<CMD>GitConflictPrevConflict<CR>")
+nmap("<leader>cl", "<CMD>GitConflictListQf<CR>")
 
 -- Folding
 nmap("ft", "vatzf<CR>")
@@ -42,11 +48,16 @@ nmap("<Leader><Leader>", "<CMD>Telescope buffers<CR>")
 -- Custome
 vmap("<S-j>", ":m '>+1<CR>gv=gv") -- move line bottom
 vmap("<S-k>", ":m '<-2<CR>gv=gv") -- move line top
-nmap("<leader>ww", "<CMD>w<CR>")  -- save
+nmap("<leader>w", "<CMD>w<CR>")
+nmap("<leader>q", "<CMD>q<CR>")
 nmap("r", "<C-r>")                -- undo
 nmap("<leader>a", "gg<S-v>G")
 nmap("<Esc>", ":noh<CR>")         -- clear higlight and back to normal
 nmap("<C-e>", [[:s/\([a-zA-Z]\)\(-\)\([a-zA-Z]\)/\1\u\3/g<CR>]]) -- kebab-case to camelCase
+nmap("<leader>n", [[:%s/\d\+/number/g]])                -- 2 -> number
+nmap("<leader>s", [[:%s/"[^"]*"/string/g]])             -- "hello" -> string
+nmap("<leader>m", [[:%s/\v(true|false)/boolean/g<CR>]]) -- true -> boolean
+nmap("<S-s>/", [[:%s//<left>]])
 
 -- Auto Pairs
 imap("$", "${}<left>")
@@ -57,20 +68,8 @@ imap("<", "<><left>")
 imap("'", "''<left>")
 imap('"', '""<left>')
 
--- Replace
-nmap("<leader>n", [[:%s/\d\+/number/g]])                -- 2 -> number
-nmap("<leader>s", [[:%s/"[^"]*"/string/g]])             -- "hello" -> string
-nmap("<leader>m", [[:%s/\v(true|false)/boolean/g<CR>]]) -- true -> boolean
-nmap("<S-s>/", [[:%s//<left>]])
-
--- Git
-nmap("<leader>gd", "<CMD>Gitsign toggle_deleted<CR>")
-nmap("<leader>gcl", "<CMD>Gitsign toggle_current_line_blame<CR>")
-nmap("<leader>gbl", "<CMD>Gitsign blame_line<CR>")
-nmap("<leader>c0", "<CMD>GitConflictChooseNone<CR>")
-nmap("<leader>cb", "<CMD>GitConflictChooseBoth<CR>")
-nmap("<leader>co", "<CMD>GitConflictChooseOurs<CR>")
-nmap("<leader>ct", "<CMD>GitConflictChooseTheirs<CR>")
-nmap("<leader>cn", "<CMD>GitConflictNextConflict<CR>")
-nmap("<leader>cp", "<CMD>GitConflictPrevConflict<CR>")
-nmap("<leader>cl", "<CMD>GitConflictListQf<CR>")
+-- Center Screen on Jumping
+nmap("n", "nzzzv")
+nmap("N", "Nzzzv")
+nmap("<C-d>", "<C-d>zz")
+nmap("<C-u>", "<C-u>zz")
